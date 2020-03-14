54cd0192e86fce10038b00688e793ad0cfd53eba
Bibek Bhujel <bibekbhujel75@outlook.com>
2020-03-14 17:44:19 +0545

    new changes

--- docker-node-app/app.js
+++ docker-node-app/app.js
@@ -1,10 +1,8 @@
-const express = require ("express");
-const app = express();
+const express = require ('express');
+const app = express()
 
-app.get("/", function(req, res){
-    res.send("Hello from docker");
-});
+app.get("/", (req, res) => res.send('hello from docker'));
 
-app.listen(3000, function(){
+app.listen(3000, () => {
     console.log("Listening to port 3000");
-});
+})
